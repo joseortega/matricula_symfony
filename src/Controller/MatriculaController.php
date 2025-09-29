7b0ca55fde60ab9868fb45fe1d2275794dfea8e6
joseortega <josheorteg@gmail.com>
2025-09-29 17:41:17 -0500

    Add Excel Report

--- src/Controller/MatriculaController.php
+++ src/Controller/MatriculaController.php
@@ -13,6 +13,7 @@ use App\Repository\ParaleloRepository;
 use App\Repository\PeriodoLectivoRepository;
 use App\Service\ReportService;
 
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -25,6 +26,10 @@ use Doctrine\ORM\Query;
 use JMS\Serializer\SerializerInterface;
 use JMS\Serializer\DeserializationContext;
 use Knp\Component\Pager\PaginatorInterface;
+
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use App\Service\ReportExcelService;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 #[Route('api')]
 class MatriculaController extends AbstractController
@@ -41,6 +46,7 @@ class MatriculaController extends AbstractController
         private GradoEscolarRepository $gradoEscolarRepository,
         private ParaleloRepository $paraleloRepository,
         private EstadoMatriculaRepository $estadoMatriculaRepository,
+        private ReportExcelService $reportExcelService,
     ){
     }
     
@@ -280,7 +286,7 @@ class MatriculaController extends AbstractController
         $paraleloFilter = "Todos";
         if($paraleloId){
             $paralelo = $this->paraleloRepository->find($paraleloId);
-            $paraleloFilter = $paraleloId ? $paralelo->getDescripcion(): "No Encontrado";
+            $paraleloFilter = $paralelo ? $paralelo->getDescripcion(): "No Encontrado";
         }
 
         $estadoMatriculasIds = [];
@@ -367,5 +373,92 @@ class MatriculaController extends AbstractController
         if($isLegalizada !== $matricula->isLegalizada()){
             if($isLegalizada){}
         }
+    }
+
+    #[Route('/excel-matricula-list', name: 'app_matricula_excel_matricula_list', methods: ['GET'])]
+    public function excelFilterList(Request $request): Response
+    {
+        $periodoLectivoId = $request->query->get('periodo_lectivo');
+        $gradoEscolarId = $request->query->get('grado_escolar');
+        $paraleloId = $request->query->get('paralelo');
+        $estadoMatriculas = $request->query->get('estado_matriculas');
+        $searchTerm = $request->query->get('search_term');
+
+        $periodoFilter = "Todos";
+        if($periodoLectivoId){
+            $periodoLectivo = $this->periodoLectivoRepository->find($periodoLectivoId);
+            $periodoFilter = $periodoLectivo ? $periodoLectivo->getDescripcion() : "No Encontrado";
+        }
+
+        $gradoEscolarFilter = "Todos";
+        if($gradoEscolarId){
+            $gradoEscolar = $this->gradoEscolarRepository->find($gradoEscolarId);
+            $gradoEscolarFilter = $gradoEscolar ? $gradoEscolar->getDescripcion(): "No Encontrado";
+        }
+
+        $paraleloFilter = "Todos";
+        if($paraleloId){
+            $paralelo = $this->paraleloRepository->find($paraleloId);
+            $paraleloFilter = $paralelo ? $paralelo->getDescripcion(): "No Encontrado";
+        }
+
+        $estadoMatriculasIds = [];
+
+        if($estadoMatriculas){
+            $estadoMatriculasIds = explode(',', $estadoMatriculas);
+            // 👉 convierte "1,3,4" en ["1","3","4"]
+            $estadoMatriculasIds = array_map('intval', $estadoMatriculasIds);
+            // 👉 convierte a enteros [1,3,4]
+        }
+
+        $estadoMatriculaFilter = [];
+        if (!empty($estadoMatriculasIds)) { // ahora es un array
+            foreach ($estadoMatriculasIds as $id) {
+                $estado = $this->estadoMatriculaRepository->find($id);
+                if ($estado) {
+                    $estadoMatriculaFilter[] = $estado->getDescripcion();
+                } else {
+                    $estadoMatriculaFilter[] = "No Encontrado";
+                }
+            }
+        }
+
+        $searchFilter = $searchTerm ?: "Ninguno";
+
+
+        $query = $this->matriculaRepository->findAllQuery(
+            $periodoLectivoId,
+            $gradoEscolarId,
+            $paraleloId,
+            $estadoMatriculasIds,
+            $searchTerm
+        );
+
+        $spreadsheet = $this->reportExcelService->printMatriculaList($periodoFilter,
+                                                                    $gradoEscolarFilter,
+                                                                    $paraleloFilter,
+                                                                    $estadoMatriculaFilter,
+                                                                    $searchFilter,
+                                                                    $query->getResult());
+        $writer = new Xlsx($spreadsheet);
+
+        $response = new StreamedResponse(function() use ($writer) {
+            $writer->save('php://output');
+        });
+
+        $response->headers->set(
+            'Content-Type',
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        );
+        $response->headers->set(
+            'Content-Disposition',
+            'attachment;filename="reporte_matriculas.xlsx"'
+        );
+
+        $response->headers->set('Cache-Control', 'max-age=0');
+
+        $response->setPrivate();
+
+        return $response;
     }
 }
---
+++ src/Service/ReportExcelService.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace App\Service;
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use App\Entity\Matricula;
+
+
+class ReportExcelService
+{
+    public function __construct()
+    {
+
+    }
+
+    public function printMatriculaList(
+        string $periodoFilter,
+        string $gradoFilter,
+        string $paraleloFilter,
+        array $estadoMatriculaFilter,
+        string $searchFilter,
+        array $matriculas): Spreadsheet
+    {
+
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+
+
+        //Encabezado
+        $sheet->getStyle('A1:A5')->getFont()->setBold(true);
+
+        $sheet->mergeCells('A1:B1');
+        $sheet->setCellValue('A1', "Periodo Lectivo");
+
+        $sheet->mergeCells('A2:B2');
+        $sheet->setCellValue('A2', "Grado Escolar");
+
+        $sheet->mergeCells('A3:B3');
+        $sheet->setCellValue('A3', "Paralelo");
+
+        $sheet->mergeCells('A4:B4');
+        $sheet->setCellValue('A4', "Estado");
+
+        $sheet->mergeCells('A5:B5');
+        $sheet->setCellValue('A5', "Termino de Busqueda");
+
+        $sheet->setCellValue('C1', $periodoFilter);
+        $sheet->setCellValue('C2', $gradoFilter);
+        $sheet->setCellValue('C3', $paraleloFilter);
+        if(!empty($estadoMatriculaFilter)){
+            // Convertimos el array en string separado por coma
+            $estadoStr = implode(', ', $estadoMatriculaFilter);
+            $sheet->setCellValue('C4', $estadoStr);
+        }else{
+            $sheet->setCellValue('C4', "Todos");
+        }
+        $sheet->setCellValue('C5', $searchFilter);
+
+        $sheet->getStyle('A7:J7')->getFont()->setBold(true);
+
+        //encabezado de Tabla
+        $sheet->setCellValue("A7", "Nro");
+        $sheet->setCellValue("B7", "Identificación");
+        $sheet->setCellValue("C7", "Nombres");
+        $sheet->setCellValue("D7", "Sexo");
+        $sheet->setCellValue("E7", "fecha de Nacimiento");
+        $sheet->setCellValue("F7", "Talla Uniforme");
+        $sheet->setCellValue("G7", "Periodo Lectivo");
+        $sheet->setCellValue("H7", "Grado");
+        $sheet->setCellValue("I7", "Estado");
+        $sheet->setCellValue("J7", "Consta-CAS?");
+        $sheet->setCellValue("K7", "Está-Legalizada?");
+
+        $this->estiloEncabezadoTabla($sheet, 'A7:K7');
+
+        //Datos de tabla
+        $row = 8;
+        $count = 1;
+
+        foreach (range('A', 'K') as $col) {
+            $sheet->getColumnDimension($col)->setAutoSize(true);
+        }
+
+        foreach ($matriculas as $matricula) {
+            $sheet->setCellValue('A'.$row, $count);
+            $sheet->setCellValue('B'.$row, $matricula->getEstudiante()->getIdentificacion());
+            $sheet->setCellValue('C'.$row, $matricula->getEstudiante()->getApellidos()." ".$matricula->getEstudiante()->getNombres());
+            $sheet->setCellValue('D'.$row, $matricula->getEstudiante()->getSexo());
+            $sheet->setCellValue('E'.$row, $matricula->getEstudiante()->getFechanacimiento()->format('d-m-Y'));
+            if($matricula->getEstudiante()->getUniformeTalla() !== null){
+                $sheet->setCellValue('F'.$row, $matricula->getEstudiante()->getUniformeTalla()->getDescripcion());
+            }else{
+                $sheet->setCellValue('F'.$row, "");
+            }
+            $sheet->setCellValue('G'.$row, $matricula->getPeriodoLectivo()->getDescripcion());
+            $sheet->setCellValue('H'.$row, $matricula->getGradoEscolar()->getDescripcion());
+            $sheet->setCellValue('I'.$row, $matricula->getEstadoMatricula()->getDescripcion());
+            $sheet->setCellValue('J'.$row, $matricula->isInscritoSistemaPublico());
+            $sheet->setCellValue('K'.$row, $matricula->isLegalizada());
+
+            $row++;
+            $count++;
+        }
+
+        // La tabla empieza en la fila 1 (con encabezados)
+        $startRow = 7;
+
+        // La última fila de datos
+        $endRow = $row - 1;
+
+        // Definir el rango
+        $range = 'A' . $startRow . ':K' . $endRow;
+
+        $this->estiloCeldaTabla($sheet, $range);
+
+        return $spreadsheet;
+
+    }
+
+    public function estiloEncabezadoTabla(Worksheet $sheet, string $range = ''){
+
+
+        // Estilo para encabezados
+        $headerStyle = [
+            'font' => [
+                'bold' => true, // Negrita
+                'color' => ['argb' => '000000'], // Texto negro
+            ],
+            'fill' => [
+                'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                'startColor' => [
+                    'argb' => 'FFD9D9D9', // Gris claro
+                ],
+            ],
+            'alignment' => [
+                'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT,
+                'vertical'   => \PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER,
+            ],
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                    'color' => ['argb' => '000000'],
+                ],
+            ],
+        ];
+
+        // Aplicar estilo al rango de encabezados
+        $sheet->getStyle($range)->applyFromArray($headerStyle);
+    }
+
+    public function estiloCeldaTabla(Worksheet $sheet, $range = 'A7:E7')
+    {
+        // Estilos de borde
+        $styleArray = [
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                    'color' => ['argb' => '000000'],
+                ],
+            ],
+            'alignment' => [
+                'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT,
+                'vertical'   => \PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER,
+            ],
+        ];
+
+        // Aplicar a toda la tabla
+        $sheet->getStyle($range)->applyFromArray($styleArray);
+    }
+
+}
